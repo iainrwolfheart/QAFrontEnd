b9e93bc0af24c8754fe5cdfaaf322d5bf30f64c0
iainrwolfheart <iainrgorrie@gmail.com>
2019-08-01 09:40:56 +0100

    Current/Future split code added on Listings Page

--- src/pages/ListingsPage.js
+++ src/pages/ListingsPage.js
@@ -4,10 +4,9 @@ import '../App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import FilmInfo from './FilmInfo';
-import Moment from 'moment';
+import moment from 'moment';
 
 export default class ListingsPage extends React.Component {
-    
     constructor(props) {
         super(props);
 
@@ -25,21 +24,24 @@ export default class ListingsPage extends React.Component {
                 isLoaded: true,
             });
         })
-        console.log(this.state.films);
-        // console.log('MOMENT', Moment(this.state.films[0].releaseDate));
     }
 
     render() {
         return (
             <div>
                 { this.state.films.map(item => {
-                    console.log(Moment(item.releaseDate));
+                    const cutOff = moment("2019-08-25", "YYYY-MM-DD");
+                    const relDate = moment(item.releaseDate, "YYYY-MM-DD");
+                    const futList = relDate.isAfter(cutOff);
+                    const curList = relDate.isBefore(cutOff);
+                    
+                    if (curList) {
                     return (
-                        <div key={item._id}>
+                        <div key={item.id}>
                             <img src={item.portImage} />
                             <h1>{item.title}</h1>
                             <h2>Directed by {item.director}</h2>
-                            <h2>Release Date: {item.releaseDate}, cert: {item.cert}</h2>
+                            <h2>Release Date: {relDate.format('DD/MM/YYYY')}, cert: {item.cert}</h2>
                             <Link to={{
 								pathname: `book/chooseshowing/${item.id}`,
 								state: {
@@ -53,54 +55,9 @@ export default class ListingsPage extends React.Component {
                                 }
                             }}><button type='button'>More Info</button></Link>
                         </div>
-                    );
-                })}
+                    )}
+                })};
             </div>
-        )
+        );
     }
 }
-
-// {
-//     '_id': 'gjpowegjwpo',
-//     'image': DoraLS,
-//     'title': 'Dora And The Lost City Of Gold',
-//     'cert': 'PG',
-//     'releaseDate': '16/8/19',
-//     'cast': 'Eugenio Derbez, Isabela Moner, Jeffrey Wahlberg',
-//     'director': 'James Bobin',
-//     'runTime': '1hr 40m',
-//     'description': "Having spent most of her life exploring the jungle with her parents, nothing could prepare Dora (Isabela Moner) for her most dangerous adventure ever - high school."
-// },
-// {
-//     '_id': 'khperokhopek',
-//     'image': OnceUponPT,
-//     'title': 'Once Upon A Time In Hollywood',
-//     'cert': '15',
-//     'releaseDate': '16/8/19',
-//     'cast': 'Brad Pitt, Leonardo DiCaprio, Margot Robbie, Al Pacino',
-//     'director': 'Quentin Tarantino',
-//     'runTime': '2hr 45m',
-//     'description': "Quentin Tarantino's Once Upon a Time in... Hollywood visits 1969 Los Angeles, where everything is changing, as TV star Rick Dalton (Leonardo DiCaprio) and his longtime stunt double Cliff Booth (Brad Pitt) make their way around an industry they hardly recognise anymore."
-// },
-// {
-//     '_id': 'agnmieojgie',
-//     'image': TellItPT,
-//     'title': 'Tell It To The Bees',
-//     'cert': '15',
-//     'releaseDate': '26/7/19',
-//     'cast': 'Anna Paquin, Gregor Selkirk',
-//     'director': 'Annabel Jankel',
-//     'runTime': '1hr 48m',
-//     'description': "Dr Jean Markham (Paquin) returns to the town she left as a teenager to take over her late father's medical practice. When a school-yard scuffle lands Charlie (Gregor Selkirk) in her surgery, she invites him to visit the hives in her garden and tell his secrets to the bees, as she once did."
-// },
-// {
-//     '_id': 'gjwepogjioej',
-//     'image': AnnabellePT,
-//     'title': 'Annabelle',
-//     'cert': '15',
-//     'releaseDate': '10/7/19',
-//     'cast': 'Mckenna Grace, Patrick Wilson, Vera Farmiga',
-//     'director': 'Gary Dauberman',
-//     'runTime': '1hr 46m',
-//     'description': "Determined to keep Annabelle from wreaking havoc, demonologists Ed and Lorraine Warren bring the possessed doll to the locked artifacts room in their house, placing her 'safely' behind sacred glass and enlisting a priest's holy blessing..."
-// }
